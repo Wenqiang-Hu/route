df585cbaf88f8a17b13c3f56025de02a38cb55c1
Wenqiang-Hu <63572326+Wenqiang-Hu@users.noreply.github.com>
2023-07-13 14:55:39 -0700

    Add: router

--- src/component/app.jsx
+++ src/component/app.jsx
@@ -1,10 +1,26 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import NavBar from "./navbar";
+import Home from "./home";
+import Linux from "./linux";
+import Django from "./django";
+import Web from "./web";
+import { Routes, Route } from "react-router-dom";
 
 class App extends Component {
-    state = {  } 
-    render() { 
-        return (<h1>hello</h1>);
+    state = {};
+    render() {
+        return (
+            <React.Fragment>
+                <NavBar />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/linux" element={<Linux />} />
+                    <Route path="/django" element={<Django />} />
+                    <Route path="/web" element={<Web />} />
+                </Routes>
+            </React.Fragment>
+        );
     }
 }
- 
+
 export default App;
